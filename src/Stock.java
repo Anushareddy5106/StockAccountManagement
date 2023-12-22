866e59f662aea9d309aaa0bbeb4dcfc8a8d23bbe
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-22 21:48:19 +0530

    StockPortfolio with Stock,StockPortfolio classes included

---
+++ src/Stock.java
@@ -0,0 +1,15 @@
+class Stock {
+    String stockName;
+    int numberOfShares;
+    double sharePrice;
+
+    public Stock(String stockName, int numberOfShares, double sharePrice) {
+        this.stockName = stockName;
+        this.numberOfShares = numberOfShares;
+        this.sharePrice = sharePrice;
+    }
+
+    public double calculateStockValue() {
+        return numberOfShares * sharePrice;
+    }
+}
